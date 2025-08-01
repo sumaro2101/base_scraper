c03c44e69fc7e8ea55b54556f93796c36d6d6dea
Alex Pavlov <yukiu217@gmail.com>
2025-08-01 21:10:15 +0300

    add more checks to env file

--- common/common.py
+++ common/common.py
@@ -18,4 +18,5 @@ class ErrorCodeProgram(str, Enum):
     ENV_NOT_FOUND = 'Env {0} not found.'
     BROWSER_NOT_PROVIDE = '{0} is not provide.'
     WRONG_PROXY_ERROR = 'Format Proxy IP is wrong - {0}. Check your ENV file.'
-    WRONG_FORMAT_LIST_ERROR = 'JSON can not decode variable {0}, must to be list format data. Check your ENV file.'
+    JSON_DECODE_ENV_ERROR = 'JSON can not decode variable {0}, must to be list format data. Check your ENV file.'
+    WRONG_FORMAT_LIST_ERROR = '{0}, must to be list format data. Check your ENV file.'
--- config/settings.py
+++ config/settings.py
@@ -36,9 +36,11 @@ class ProxySettings(BaseModel):
     """
     try:
         PROXIES_URLS: list[str] = json.loads(os.getenv('PROXIES_URLS', default='[]'))
-    except json.decoder.JSONDecodeError as e:
-        raise WrongFormatListError(ErrorCodeProgram.WRONG_FORMAT_LIST_ERROR.format('PROXIES_URLS'))
-    REGEX_PROXY_PATTERN: str = r'^(.*:.*@){0,1}([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}:[0-9]{1,4})(:[0-9]{1,4})?$'
+    except json.decoder.JSONDecodeError:
+        raise WrongFormatListError(ErrorCodeProgram.JSON_DECODE_ENV_ERROR.format('PROXIES_URLS'))
+    REGEX_ENTER_DATA_PATTERN: str = r'(.{1,}:.{1,}@)'
+    REGEX_IP_PORT_PATTERN: str = r'([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}:[0-9]{1,4})(:[0-9]{1,4})?'
+    REGEX_PROXY_PATTERN: str = r'^' + REGEX_ENTER_DATA_PATTERN + r'{0,1}' + REGEX_IP_PORT_PATTERN + r'$'
     IPV4: bool = False if os.getenv('IPV4', default=True) == 'False' else True
     ALONE_PROXY: bool = False if os.getenv('IPV4', default=True) == 'False' else True
 
@@ -46,13 +48,11 @@ class ProxySettings(BaseModel):
     def check_proxy_list(self):
         list_of_proxy: str | list = self.PROXIES_URLS
         logger.debug(f'Get list from env {list_of_proxy}')
-        if isinstance(list_of_proxy, str):
-            if not list_of_proxy.startswith('[') or not list_of_proxy.endswith(']'):
-                raise WrongFormatListError(ErrorCodeProgram.WRONG_FORMAT_LIST_ERROR.format(list_of_proxy))
-            list_of_proxy = [proxy.strip() for proxy in list_of_proxy.strip('[').strip(']').split(',')]
+        if not isinstance(list_of_proxy, list):
+            raise WrongFormatListError(ErrorCodeProgram.WRONG_FORMAT_LIST_ERROR.format('PROXIES_URLS'))
         for proxy in list_of_proxy:
             if re.match(self.REGEX_PROXY_PATTERN, proxy) is None:
-                logger.debug(f'Enter Wrond data {proxy} in regex {self.REGEX_PROXY_PATTERN}')
+                logger.debug(f'Enter Wrong data {proxy} in regex {self.REGEX_PROXY_PATTERN}')
                 raise WrongProxyError(ErrorCodeProgram.WRONG_PROXY_ERROR.format(proxy))
         return self
 
